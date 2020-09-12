d3f3ceb1ee6d635054fa52f9482be25a547f3b88
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2020-09-12 12:59:27 -0400

    add the abliity to use PPA and snap packages

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ubuntu-env
-A couple of scripts that I use to get up and running on a new Ubuntu install. 
+Installs common packages that I use to get up and running with a new Ubuntu install. 
+
+This will use `apt` to install any packages in the `apt-packs.txt` file.
+If you need to add a `ppa` cert to your machine, then it will also handle
+that, just throw it in the same text file (see Lutris for example).
+
+This also downloads [OhMyZsh](https://github.com/ohmyzsh/ohmyzsh) and sets
+it as the default via `chsh`
+
+```
+OVERVIEW: Installs any packages and adds any PPA's in the 'apt-packs.txt' file.
+ 
+OPTIONS:
+	-h	Help. Prints this message!
+	-d	Dry Run. If this flag is set then nothing will actually be installed, only printed out to the screen
+```
+
+Probably would have been easier to do this in python, but python is one of the packages
+that this installs so ¯\_(ツ)_/¯
---
+++ go.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# If a dry run is enabled then we will just print out what will happen
+# instead of actually installing anything. Useful for debugging and 
+# UX! 
+dry_run_flag=false
+has_snap=false
+
+print_usage() {
+  echo "OVERVIEW: Installs any packages and adds any PPA's in the 'apt-packs.txt' file."
+  echo " "
+  echo "OPTIONS:"
+    printf "\t-h\tHelp. Prints this message!\n"
+  printf "\t-d\tDry Run. If this flag is set then nothing will actually be installed, only printed out to the screen\n"
+}
+
+# parse cmd args
+while getopts 'dh' flag; do
+  case "${flag}" in
+    d) dry_run_flag=true ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+if [ "$dry_run_flag" = true ] ; then
+  printf "Doing a dry run! Nothing will actually be installed! :)\n\n"
+fi
+
+## Test to see if snap is installed on this machine
+snap -h > /dev/null
+if [ $? -eq 0 ]; then
+  echo Snap detected. Noice
+  has_snap=true
+fi
+
+# If this machine is missing snap, then attempt to install it
+if [ "$has_snap" = false ] ; then
+  printf "NOTE: snap is not installed on this machine! Installing...\n\n"
+    if [ "$dry_run_flag" = false ] ; then
+      apt install snapd
+      snap -h > /dev/null
+      if [ $? -eq 0 ]; then
+        echo Snap installed successfully!
+        has_snap=true
+      else
+        echo Snap could not be isntalled!
+      fi
+    fi
+  
+fi
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ] && [ "$dry_run_flag" = false ] ; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+outText='output.log'
+packagesFile='packs.txt'
+ppa_prefix='ppa:'
+snap_prefix='snap '
+
+# Gotta get those tastey updates first
+echo apt-update and ugprade
+if [ "$dry_run_flag" = false ] ; then
+  apt-get update -y > $outText
+  apt-get upgrade -y > $outText
+fi
+
+# For each line in the packages file
+while IFS="" read -r p || [ -n "$p" ]
+do
+  # If it starts with "ppa:" than we need to add the repo key
+  if [[ $p = $ppa_prefix* ]] 
+  then
+	  printf 'Adding PPA key\t \"%s\"...\n' "$p"
+    if [ "$dry_run_flag" = false ] ; then
+      add-apt-repository $p > $outText
+      apt update -y > $outText
+    fi
+  elif [[ $p = $snap_prefix* ]] && [ "$has_snap" = true ] 
+  then 
+      snapPkg=$(cut -d " " -f2- <<< "$p")
+      printf 'Snap install\t \"%s\"...\n' "$snapPkg"
+      if [ "$dry_run_flag" = false ] ; then
+        snap install $snapPkg > $outText
+      fi
+  # Otherwise we can just apt-install it
+  else
+	  printf 'Apt Install\t \"%s\" ...\n' "$p"
+    
+    if [ "$dry_run_flag" = false ] ; then
+      apt-get install $p -y  > $outText
+    fi
+  fi	  
+done < $packagesFile
+
+
+# ZSH install!! Weee
+echo Set ZSH as default! 
+if [ "$dry_run_flag" = false ] ; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  chsh -s $(which zsh)
+fi
